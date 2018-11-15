f8f6da83885997ed66b7017a569573b447e74c92
NathanielYoung <young.nathaniel95@gmail.com>
2018-11-15 11:03:53 -0700

    50% done with tutorial

---
+++ pages/views.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+
+# Create your views here.
+# creates a new page
+def home(request):
+    return render(request, "home.html", {})
+
+def about(request):
+    return render(request, "about.html", {})
