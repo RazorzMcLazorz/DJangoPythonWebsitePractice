fdcd44f0629c8723c9306a0536b37dbcf091fa9c
NathanielYoung <young.nathaniel95@gmail.com>
2018-11-16 08:58:58 -0700

    version change

--- pages/urls.py
+++ pages/urls.py
@@ -1,5 +1,8 @@
-from django.urls import path
+from django.urls import include, path
+from . import views
 
 urlpatterns = [
-    
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
 ]
